3581ef390dad24472885ae26dde4da0abe900164
x24ken <x24ken@gmail.com>
2024-01-13 13:41:34 +0900

    Add index.html and serve static files from public folder

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res, next) => {
-    res.end("Top page")
-})
+app.use(express.static('public'));
 
-// サーバーを起動する
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
