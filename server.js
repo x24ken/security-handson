d18cd14fc1bfffa0f7b3f4c30ebdda8094c7a696
x24ken <x24ken@gmail.com>
2024-01-13 14:42:12 +0900

    Add route for top page

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.get('/', (req, res, next) => {
+    res.end('Top Page!')
+});
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
