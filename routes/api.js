985e509f28945f980228d2d94ecfd27e85b3c6d3
x24ken <x24ken@gmail.com>
2024-01-14 11:04:08 +0900

    Add X-Timestamp header and handle empty message in GET request

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send({ message: "Hello"})
+    res.setHeader("X-Timestamp", Date.now())
+    let message = req.query.message
+    const lang = req.headers["X-Lang"]
+
+    if (message === ""){
+        res.status(400);
+        if (lang === "en") {
+            message = "Bad Request"
+        } else {
+            message = "不正なリクエストです"
+        }
+    }
+    res.send({ message })
 });
+
+router.use(express.json())
+router.post("/", (req, res) => {
+    const body = req.body;
+    console.log(body);
+    res.end()
+})
 
 module.exports = router;
