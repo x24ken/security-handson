f2441d80e32eb61d4420ce11d6c3ae5255db3eed
x24ken <x24ken@gmail.com>
2024-01-15 14:59:46 +0900

    Add CORS handling for specified origins

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const allowList = [
+    "http://localhost:3000",
+    "http://site.example:3000"
+]
+
+router.use((req, res, next) => {
+    // Originヘッダが存在している、かつリクエスト許可するリスト内にOriginヘッダの値が含まれているかチェック
+    if (req.headers.origin && allowList.includes(req.headers.origin)) {
+        res.header("Access-Control-Allow-Origin", req.headers.origin)
+    }
+    if (req.method === "OPTIONS") {
+        // プリフライトリクエストで、許可するヘッダを明示する
+        res.header("Access-Control-Allow-Headers", "X-Token")
+    }
+    next();
+})
+
 router.get('/', (req, res) => {
     res.setHeader("X-Timestamp", Date.now())
     let message = req.query.message
