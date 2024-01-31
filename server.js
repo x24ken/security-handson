1f0f7441adcc346e5ae1f07fc037dfb73110b0b7
x24ken <x24ken@gmail.com>
2024-01-31 09:55:18 +0900

    Add CSP validation page to server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const api = require('./routes/api');
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.use(express.static('public'));
 
 app.use('/api', api);
@@ -10,6 +12,10 @@ app.use('/api', api);
 app.get('/', (req, res, next) => {
     res.end('Top Page!')
 });
+
+app.get("/csp", (req, res) => {
+    res.render("csp")
+})
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
